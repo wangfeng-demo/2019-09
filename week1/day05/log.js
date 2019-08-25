9bcff02fcf7540a8f48ea42ab0345fd81f9e0595
wangfeng-demo <624763084@qq.com>
2019-08-25 21:11:20 +0800

    update

---
+++ week1/day05/log.js
@@ -0,0 +1,23 @@
+var logModal =(function(){
+   var name = 123;
+   var age = 234;
+   var f = function(){
+   console.log('注册模块')
+   console.log(name,age)
+}
+return{
+    f
+}
+})();
+
+var logM =(function(){
+   var name = 123;
+   var age = 234;
+   var f = function(){
+   console.log('登录模块')
+   console.log(name,age)
+}
+return{
+    f
+}
+})()
